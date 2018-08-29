493d0ac111daeaa2b973f019b1ab033fb69acb7b
dr1ngo <37157623+dr1ngo@users.noreply.github.com>
2018-08-29 16:53:58 +0500

    Add files via upload

--- cd/js/common.js
+++ cd/js/common.js
@@ -1,4 +1,19 @@
 $(function() {
+    $(window).scroll(function() {
+
+        if($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+     
+    $('#toTop').click(function() {
+        $('body,html').animate({scrollTop:0},800);
+    });
+});
+
+$(function() {
 
     $(".header-scroll").on("click","a", function (event) {
         //отменяем стандартную обработку нажатия по ссылке
@@ -12,6 +27,17 @@ $(function() {
     }),
 
 	$(".navigation").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    }),
+
+    $(".navigation--mobile").on("click","a", function (event) {
         //отменяем стандартную обработку нажатия по ссылке
         event.preventDefault();
         //забираем идентификатор бока с атрибута href
