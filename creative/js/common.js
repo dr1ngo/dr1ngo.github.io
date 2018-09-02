fc3cc05967d31b0bb2621fb36eb334e1e90ed200
dr1ngo <37157623+dr1ngo@users.noreply.github.com>
2018-09-02 19:18:48 +0500

    Add files via upload

--- creative/js/common.js
+++ creative/js/common.js
@@ -1,26 +1,31 @@
 $(function() {
-    $(window).scroll(function() {
-
-        if($(this).scrollTop() > 610) {
-            $('.header').fadeOut();
-        } else {
-            $('.header').fadeIn();
-        }
-    }),
-     
-    $('#toTop').click(function() {
-        $('body,html').animate({scrollTop:0},800);
-    }),
-
     $('.why-slider').slick({
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
       nextArrow: '<a class="arrow arrow-next">',
       prevArrow: '<a class="arrow arrow-prev">'
-    }),
-    
+    });
+});
+
+
+$(function() {
+  if($(window).width() > 991) {
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 610) {
+        $('.header').addClass("notActive");
+      }
+      if($(this).scrollTop() == 0) {
+        $('.header').removeClass("notActive");
+      }
+    })
+  }
+});
+
+$(function() {
+  if($(window).width() < 991) {
     jQuery('.logo').click(function(){
       $('.header').slideToggle("slow");
     });
+  }
 });
