977b2132489439fafdd994f8f8b09862e4da757a
dr1ngo <37157623+dr1ngo@users.noreply.github.com>
2018-09-03 20:22:05 +0500

    Add files via upload

--- creative/js/common.js
+++ creative/js/common.js
@@ -10,9 +10,9 @@ $(function() {
 
 
 $(function() {
-  if($(window).width() > 991) {
+  if($(window).width() > 1072) {
     $(window).scroll(function() {
-      if($(this).scrollTop() > 610) {
+      if($(this).scrollTop() > 500) {
         $('.header').addClass("notActive");
       }
       if($(this).scrollTop() == 0) {
@@ -23,9 +23,17 @@ $(function() {
 });
 
 $(function() {
-  if($(window).width() < 991) {
+  if($(window).width() < 1072) {
     jQuery('.logo').click(function(){
       $('.header').slideToggle("slow");
     });
   }
+});
+
+$(function() {
+  jQuery(this).click(function(){
+    $('.dot-content').slideToggle("slow");
+    $('.dot-header').toggleClass("dot-header--active");
+    $('.dot__icon').toggleClass("dot__icon--active");
+  });
 });
